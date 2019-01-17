5f60ba92a2ea874cacdd684fa35fdc76568922fd
Freddy <ymfreddy@gmail.com>
2019-01-17 03:23:56 -0400

    Primer commit

---
+++ app-old.js
@@ -0,0 +1,13 @@
+const http = require('http');
+http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    let salida = {
+        nombre: 'juan',
+        edad: 35,
+        url: req.url
+    }
+    res.write(JSON.stringify(salida));
+    res.end();
+}).listen(8080);
+
+console.log('escuchando el puerto 8080');
